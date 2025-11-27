f4d095f48f48a5c8865f97f958509e53bcc6d7ef
Walshwade-dev <mainace8@gmail.com>
2025-11-27 22:07:37 +0300

    fixed card rendering on small screen

--- src/main.js
+++ src/main.js
@@ -4,3 +4,11 @@ import { renderAll } from './ui/render.js';
 
 attachUI();
 renderAll(); // initial render
+
+
+function updateVH() {
+    document.documentElement.style.setProperty('--vh', window.innerHeight * 0.01 + 'px');
+}
+
+updateVH();
+window.addEventListener('resize', updateVH);
